9311a6a86e30777989696cacf0a570fba8fc6fd2
ghostpass-jhseok <jh.seok@ghostpass.ai>
2026-03-05 15:32:29 +0900

    refactor: use binaryTarget directly as product with Firebase shim target

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "GoPassSDK",
     platforms: [.iOS(.v15)],
     products: [
-        .library(name: "GoPassSDK", targets: ["GoPassSDK"])
+        .library(name: "GoPassSDK", targets: ["GoPassSDK", "GoPassSDKFirebase"])
     ],
     dependencies: [
         .package(
@@ -15,18 +15,17 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "GoPassSDKBinary",
+            name: "GoPassSDK",
             url: "https://github.com/ghostpass-ai/ghostpass-sdk-distribution/releases/download/0.0.0/GoPassSDK.xcframework.zip",
             checksum: "1779ffb6a8a29f4fb46a8cc28529966cbba268265b635ee55a72e83974005cde"
         ),
         .target(
-            name: "GoPassSDK",
+            name: "GoPassSDKFirebase",
             dependencies: [
-                "GoPassSDKBinary",
                 .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseDatabase", package: "firebase-ios-sdk"),
             ],
-            moduleAliases: ["GoPassSDK": "GoPassAuth"]
+            path: "Sources/GoPassSDK"
         )
     ]
 )
--- Sources/GoPassSDK/Exports.swift
+++ Sources/GoPassSDK/Exports.swift
@@ -1,1 +1,1 @@
-@_exported import GoPassAuth
+// stub
