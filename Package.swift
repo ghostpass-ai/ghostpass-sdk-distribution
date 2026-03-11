f2f7516cc3884c9b62e62d191d9ed222ee039dac
ghostpass-jhseok <jh.seok@ghostpass.ai>
2026-03-11 13:44:32 +0900

    release: 0.1.3

--- Package.swift
+++ Package.swift
@@ -2,30 +2,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "GoPassSDK",
-    platforms: [.iOS(.v15)],
-    products: [
-        .library(name: "GoPassSDK", targets: ["GoPassSDK", "GoPassSDKFirebase"])
-    ],
-    dependencies: [
-        .package(
-            url: "https://github.com/firebase/firebase-ios-sdk.git",
-            "10.0.0"..<"13.0.0"
-        )
-    ],
-    targets: [
-        .binaryTarget(
-            name: "GoPassSDK",
-            url: "https://github.com/ghostpass-ai/ghostpass-sdk-distribution/releases/download/0.1.2/GoPassSDK.xcframework.zip",
-            checksum: "4b5485c641bc3fcf27b261a2ccfe87a3d09df1d9c44c0904424311102594570a"
-        ),
-        .target(
-            name: "GoPassSDKFirebase",
-            dependencies: [
-                .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseDatabase", package: "firebase-ios-sdk"),
-            ],
-            path: "Sources/GoPassSDK"
-        )
-    ]
+  name: "GoPassSDK",
+  platforms: [.iOS(.v15)],
+  products: [
+      .library(name: "GoPassSDK", targets: ["GoPassSDK"])
+  ],
+  targets: [
+      .binaryTarget(
+          name: "GoPassSDK",
+          url: "https://github.com/ghostpass-ai/ghostpass-sdk-distribution/releases/download/0.1.3/GoPassSDK.xcframework.zip",
+          checksum: "63a11461977c987a1911843a731e4249241cd511bfebada72e8096aa6ff0bc12"
+      )
+  ]
 )
