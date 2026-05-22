be205c3fb85aa9eeb67cad8596d118748a8acf26
Jihye Seok <jeje@Jihyeui-MacBookPro.local>
2026-05-22 18:33:02 +0900

    release: 1.0.3

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
       .binaryTarget(
           name: "GoPassSDK",
-          url: "https://github.com/ghostpass-ai/ghostpass-sdk-distribution/releases/download/1.0.2/GoPassSDK.xcframework.zip",
-          checksum: "0139b020831c29d8bc8f563310b05ce7f2ad751c84f1c3f607dcdb76135efd78"
+          url: "https://github.com/ghostpass-ai/ghostpass-sdk-distribution/releases/download/1.0.3/GoPassSDK.xcframework.zip",
+          checksum: "03b9bfb13670453acf35ea493564985bea2a44d90a1ade2648362a35ed336f3a"
       )
   ]
 )
